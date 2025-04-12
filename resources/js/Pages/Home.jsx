28e950ca81ef1754110d2d2407f029b94d7f686a
arman.dabir <dabirmoghadam.a@gmail.com>
2025-04-12 17:13:43 +0330

    simple change at work

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -3,6 +3,7 @@ export default function Home(){
        <div>
          <h1 className="font-iranSansBold text-3xl">شرکت فنی مهندسی اکسیال 2</h1>
          <h2 className="text-xl">ارائه کننده‌ی انواع خدمات مهندسی و تاسیسات ساختمان</h2>
+         <h3 className="text-md">این متن تست است </h3>
        </div>
     )
 }
