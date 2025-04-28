adf0398b3d17ff4ed1504a370acfec90c0e2ef72
armandabir <81634618+armandabir@users.noreply.github.com>
2025-04-28 21:56:29 +0330

    section 3 - Update Card2 and SaleAgency components for improved layout and styling; enhance navbar styles

--- resources/js/components/Card2.jsx
+++ resources/js/components/Card2.jsx
@@ -1,21 +1,23 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+
 import styles from "./../../css/styles/Card2.module.scss";
 export default function Card2({img,tilte,likes,price}){
     return (
-        <article className={styles.Card2}>
-            <div>
+        <article className={styles.card2}>
+            <div className="h-60 rounded-xl overflow-hidden">
                 <img src={img} alt="" />
             </div>
             <div>
-                <h3>{tilte}</h3>
-                <div>
-                    <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
-                    <span>{likes}</span>
+                <h3 className="my-2">{tilte}</h3>
+                <div className="w-1/2 mr-auto text-left">
+                    <span className="ml-2">{likes}</span>
+                    <FontAwesomeIcon icon={faHeart} />
                 </div>
                 <hr />  
-                <div>
-                    <span>قیمت</span> <span>{price}</span>
+                <div className="flex justify-between">
+                    <span>قیمت</span> <span>{`${price} هزار تومان`}</span>
                 </div>
             </div>
         </article>
--- resources/js/components/SaleAgency.jsx
+++ resources/js/components/SaleAgency.jsx
@@ -8,9 +8,9 @@ import Card2 from "./Card2";
 export default function SaleAgency(){
     return(
         <section className={styles.container}>
-            <div className="relative z-30 md:flex xl:flex-row flex-col flex-wrap">
+            <div className="relative z-30 md:flex xl:flex-row flex-col flex-wrap md:px-20">
                 <MySwiper 
-                className="h-full md:w-2/3 shrink-0" 
+                className="h-full md:w-3/5 shrink-0" 
                 modules={[Autoplay,Navigation, Pagination, Scrollbar]}
                 autoplay={{
                     delay:3000,
@@ -23,23 +23,23 @@ export default function SaleAgency(){
                             slidesPerView:2
                         },
                         1024:{
-                            spaceBetween:50,
+                            spaceBetween:20,
                             slidesPerView:3
                         }
                     }}
 
-                pagination={{
-                    clickable: true,
-              
-                }}
+          
 
                 sectionCard={[
-                    <Card1 className="bg-[url('/resources/assets/rectangle1.jpg')] bg-auto" icon={bolt1} title="مهندسین مشاور" desc="مهندسین مشاور با بهره‌گیری از "/>,
+                    
+                    <Card2 img="../../assets/ayegh.jpg" tilte="دیگ چگالی" likes={25} price={700}/>,
+                    <Card2 img="../../assets/ayegh.jpg" tilte="دیگ چگالی" likes={25} price={700}/>,
+                    <Card2 img="../../assets/ayegh.jpg" tilte="دیگ چگالی" likes={25} price={700}/>,
                     <Card2 img="../../assets/ayegh.jpg" tilte="دیگ چگالی" likes={25} price={700}/>
                   
                 ]}
                 />
-                <div className="md:w-1/4 mx-auto shrink-0 text-center">
+                <div className="md:w-2/5 mx-auto shrink-0 text-center">
                     <h2 className="text-3xl my-[10%] font-iranSansBold">نمایندگی فروش تجهیزات</h2>
                     <Button className="w-2/4 my-[5%] bg-orange-400 xl:p-3">مشاهده نمایندگی</Button>
                 </div>
