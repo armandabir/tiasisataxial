d130f7203ce030f0cea9208aaca1665a917a4022
armandabir <81634618+armandabir@users.noreply.github.com>
2025-05-06 00:33:11 +0330

    section 5 - completeing banner

--- resources/js/components/Approch.jsx
+++ resources/js/components/Approch.jsx
@@ -1,11 +1,29 @@
 import { BlueWhiteBg } from "./BlueWhiteBg";
 import styles from "./../../css/styles/approch.module.scss"
+import logo from "./../../assets/logo2.jpg"
 export default function Approch(){
     return(
         <div className={styles.Approch}>
             <div className={styles.banner}>
                 <div className={styles.slogan}>
-                    <h2>راه حل بودن، تعهد ماست</h2>
+                    <img className={styles.logo} src={logo} alt="" />
+                    <h2 className={styles.title}>راه حل بودن، تعهد ماست</h2>
+                    <div  className={styles.steps}>
+                        <h3>
+                            پیش از اجرا
+                            (مشاوره و طراحی)
+                        </h3>
+                        <h3>
+                            حین اجرا
+                            (اجرا و نظارت)
+                        </h3>
+                        <h3>
+                            پس از اجرا
+                            <div>
+                            (گارانتی و تعمیر و نگهداری)
+                            </div>
+                        </h3>
+                    </div>
                 </div>
             </div>
             <BlueWhiteBg className="h-4/5 w-full absolute left-1/2 transform -translate-x-1/2 z-10"/>
