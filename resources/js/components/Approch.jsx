ec59ab6a2777c5eff02c1ce9dbb1b654ffa61bf0
armandabir <81634618+armandabir@users.noreply.github.com>
2025-05-07 00:17:35 +0330

    section - 5 - completed - Refactor Approch component layout and styling for improved responsiveness and visual hierarchy

--- resources/js/components/Approch.jsx
+++ resources/js/components/Approch.jsx
@@ -9,20 +9,29 @@ export default function Approch(){
                     <img className={styles.logo} src={logo} alt="" />
                     <h2 className={styles.title}>راه حل بودن، تعهد ماست</h2>
                     <div  className={styles.steps}>
-                        <h3>
-                            پیش از اجرا
-                            (مشاوره و طراحی)
-                        </h3>
-                        <h3>
-                            حین اجرا
-                            (اجرا و نظارت)
-                        </h3>
-                        <h3>
-                            پس از اجرا
-                            <div>
-                            (گارانتی و تعمیر و نگهداری)
+                       <div className="w-2/3 md:w-full flex flex-col order-last md:order-first md:flex-row justify-around">
+                            <h3>
+                                پیش از اجرا
+                                (مشاوره و طراحی)
+                            </h3>
+                            <h3 className="md:transform md:-translate-x-6">
+                                حین اجرا
+                                (اجرا و نظارت)
+                            </h3>
+                            <h3>
+                                پس از اجرا
+                                <div>
+                                (گارانتی و تعمیر و نگهداری)
+                                </div>
+                            </h3>
+                       </div>
+                       <div className="w-1/3 md:w-full">
+                            <div className={styles.progress}>
+                                <div></div>
+                                <div></div>
+                                <div></div>
                             </div>
-                        </h3>
+                       </div>
                     </div>
                 </div>
             </div>
