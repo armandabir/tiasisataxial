9ff48d06ad011aeed564723a59b769026ecbd997
armandabir <81634618+armandabir@users.noreply.github.com>
2025-05-11 00:24:12 +0330

    section 7 - complete

--- resources/js/components/Apps.jsx
+++ resources/js/components/Apps.jsx
@@ -1,9 +1,20 @@
 import styles from "./../../css/styles/apps.module.scss"
-import { BlueWhiteBg } from "./BlueWhiteBg"
+import img0 from "./../../assets/ASHRAE_0.png"
+import img1 from "./../../assets/ASHRAE_1.png"
+import img2 from "./../../assets/ASHRAE_2.png"
+import img3 from "./../../assets/ASHRAE_3.png"
+import img4 from "./../../assets/ASHRAE_4.png"
+
 export default function Apps(){
     return (
         <section className={styles.Apps}>
-            <BlueWhiteBg className="h-full -scale-y-105 -rotate-2"/>
+            <div className={styles.container}>
+                <img src={img0} alt="" />
+                <img src={img1} alt="" />
+                <img src={img2} alt="" />
+                <img src={img3} alt="" />
+                <img src={img4} alt="" />
+            </div>
         </section>
     )
 }
