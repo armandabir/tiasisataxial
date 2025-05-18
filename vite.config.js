53734f61387acff9dadda47d1aa561bf42436a62
armandabir <81634618+armandabir@users.noreply.github.com>
2025-05-18 22:14:36 +0330

    subtract

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
+    build: {
+    outDir: 'public/build',
+    // ...
+  },
     plugins: [
         laravel({
             input: ['resources/css/index.css', 'resources/js/index.jsx'],
