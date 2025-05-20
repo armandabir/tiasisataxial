f73b07ba4947a5132b566f01ab8c48c233e614a4
armandabir <81634618+armandabir@users.noreply.github.com>
2025-05-20 23:42:05 +0330

    mobile navbar - complete Enhance Navbar component with FontAwesome icons and update mobile menu layout

--- resources/js/components/Navbar.jsx
+++ resources/js/components/Navbar.jsx
@@ -3,7 +3,9 @@ import logo from  "./../../assets/logo.png"
 import Button from "./Button"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
-
+import { faCalculator } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
 export default function Navbar(){
     return ( 
         <nav className={styles.navbar}>
@@ -38,15 +40,39 @@ export default function Navbar(){
             <ul className={`${styles.mobileMenu} md:hidden`}>
                 <li>
                     <ul>
-                         <li>خانه</li>
-                        <li>فروشگاه</li>
+                        <li>
+                            <a href="">
+                                <FontAwesomeIcon icon={faHouse}/>
+                                <p>خانه</p>    
+                            </a>
+                        </li>
+                      
+                        <li>
+                            <a href="">
+                                <FontAwesomeIcon icon={faUser} />
+                                <p>ورود</p>
+                            </a>
+                        </li>
                     </ul>
                 </li>
                
                 <li>
                     <ul>
-                        <li>ورود</li>
-                        <li>سبد خرید</li>
+                        <li>
+                            <a href="">
+                                <FontAwesomeIcon icon={faCalculator}/>
+                                <p>محاسبات</p>
+                            </a>
+                        </li>
+                        <li className="relative">
+                            <a href="">
+                                <FontAwesomeIcon icon={faShoppingCart}/>
+                                <span className="absolute -top-3 right-0 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
+                                0
+                                </span>
+                                <p>سبد خرید</p>
+                            </a>
+                        </li>
                     </ul>
                 </li>
             </ul>
