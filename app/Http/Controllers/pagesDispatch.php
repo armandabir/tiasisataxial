61472196fca9294cac01c2de641dcb932d9da553
armandabir <81634618+armandabir@users.noreply.github.com>
2025-05-27 22:28:57 +0330

    Implement index view in pagesDispatch controller and fix route for catch-all

--- app/Http/Controllers/pagesDispatch.php
+++ app/Http/Controllers/pagesDispatch.php
@@ -13,7 +13,7 @@ class pagesDispatch extends Controller
      */
     public function index()
     {
-        //
+        return view("index");
     }
 
     /**
--- vite.config.js
+++ vite.config.js
@@ -22,15 +22,8 @@ export default defineConfig({
         host: '0.0.0.0', // Use localhost or your machine's IP address
         port: 5173, // Ensure the port matches your setup
         hmr: {
-<<<<<<< HEAD
             host: '192.168.1.50', // Use localhost for HMR
-=======
-<<<<<<< HEAD
-            host: '172.28.84.80', // Use localhost for HMR
-=======
-            host: '192.168.125.184', // Use localhost for HMR
->>>>>>> f73b07ba4947a5132b566f01ab8c48c233e614a4
->>>>>>> 4b621c431797287261209f57315eea87a61e3b73
+
         },
         cors: {
             origin: '*', // Allow all origins (adjust as needed)
