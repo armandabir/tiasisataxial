5b0a047b4c443a3b89c2f8b68520c230e2708351
armandabir <81634618+armandabir@users.noreply.github.com>
2025-06-01 00:48:01 +0330

    Refactor Header component structure and styles for improved layout

--- resources/js/components/catergories/Header.jsx
+++ resources/js/components/catergories/Header.jsx
@@ -3,7 +3,23 @@ export default function Header(){
     return (
        <>
         <header className={styles.Header}>
-            <p>دسته بندی</p>
+            <div className={styles.content}>
+                 <h3>دسته بندی</h3>
+                 <ul>
+                    <li>خانه . </li>
+                    <li>دسته بندی</li>
+                 </ul>
+
+               
+            </div>
+
+             <div className={styles.searchBox}>
+                <h4>دنبال محصول خاصی هستین ؟</h4>
+                <div className={styles.search}>
+                    <input type="text" />
+                </div>
+            </div>
+
             <div className={styles.headerBg}></div>
         </header>
        </>
