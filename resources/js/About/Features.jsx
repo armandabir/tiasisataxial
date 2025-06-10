5c15f87e1569283f78e05e8e3e3a41fc0ba08c9e
armandabir <81634618+armandabir@users.noreply.github.com>
2025-06-10 23:26:26 +0330

    Add new feature cards with images and update styles for the Features component

--- resources/js/About/Features.jsx
+++ resources/js/About/Features.jsx
@@ -1,9 +1,42 @@
 import styles from "./../../css/styles/About/features.module.scss"
+import product from "././../../assets/about/contact-support.png"
+import quality from "./../../assets/about/w3c.png"
+import service from "./../../assets/about/w3c2 1.png"
+import price from "./../../assets/about/vector.png"
 export default function Features(){
     return (
         <section className={styles.features}>
             <h2>ویژگی های تیم ما</h2>
             <div className={styles.content}>
+                <div className={styles.fade}></div>
+
+                <div className={styles.card}>
+                    <div>
+                        <img src={price} alt="" />
+                    </div>
+                    <p>مناسب‌ترین قیمت نسبت به بازار</p>
+                </div>
+
+                <div className={styles.card}>
+                    <div>
+                        <img src={product} alt="" />
+                    </div>
+                    <p>تمامی محصولات دارای گارانتی</p>
+                </div>
+
+                <div className={styles.card}>
+                    <div>
+                        <img src={quality} alt="" />
+                    </div>
+                    <p>تمامی محصولات دارای گارانتی</p>
+                </div>
+
+                <div className={styles.card}>
+                    <div>
+                        <img src={service} alt="" />
+                    </div>
+                    <p>تمامی محصولات دارای گارانتی</p>
+                </div>
 
             </div>
         </section>
