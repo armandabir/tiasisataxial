721c1f186bed44907bb375df4352b377cc96c1cc
armandabir <81634618+armandabir@users.noreply.github.com>
2025-06-17 22:55:24 +0330

    Add product image and update ProductSlide component layout

--- resources/js/Product/ProductSlide.jsx
+++ resources/js/Product/ProductSlide.jsx
@@ -3,6 +3,20 @@ export default function ProductSlide(){
     return(
        <section className={styles.productSlide}>
             <img src="/assets/product/product-img.jpg" alt="" />
+            <div className={styles.btContainer}>
+                 <button>
+                    <img src="/assets/product/product-img-5.jpg" alt="" />
+                </button>
+                  <button>
+                    <img src="/assets/product/product-img-5.jpg" alt="" />
+                </button>
+                  <button>
+                    <img src="/assets/product/product-img-5.jpg" alt="" />
+                </button>
+                  <button>
+                    <img src="/assets/product/product-img-5.jpg" alt="" />
+                </button>
+            </div>
        </section> 
     )
 }
