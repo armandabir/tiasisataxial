fb3fd09b36cfda2e5aa192a1d92fd0fc775d944a
armandabir <81634618+armandabir@users.noreply.github.com>
2025-06-22 22:28:52 +0330

    Enhance Product Description layout and functionality
    - Updated ProductDesc component to include a general description section with title and paragraph.
    - Modified BlueWhiteBg component to accept an appimg prop for dynamic background image selection.
    - Adjusted styles in productdesc.module.scss for improved layout.

--- resources/js/components/BlueWhiteBg.jsx
+++ resources/js/components/BlueWhiteBg.jsx
@@ -1,12 +1,13 @@
 import img from "./../../assets/blue-white-bg.png"
+import appimgbg from "./../../assets/appsbg.png"
 import mobileimg from "./../../assets/bgwbMobile.png"
 import styles from "./../../css/styles/BlueWhiteBg.module.scss"
-export function BlueWhiteBg({className}){
+export function BlueWhiteBg({className,appimg}){
     const {innerWidth:width}=window;
         
     return(
         <div className={`${styles.container}  ${className}`}>
-            <img src={innerWidth < 640 ? mobileimg : img } alt="" />
+            <img src={innerWidth < 640 ? mobileimg : appimg ? appimgbg : img  } alt="" />
         </div>
     )
 }
