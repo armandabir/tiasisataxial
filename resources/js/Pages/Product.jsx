b1c419c25037d26946219d109c7215fde9f8a183
armandabir <81634618+armandabir@users.noreply.github.com>
2025-06-24 00:56:12 +0330

    Add ProductDetails component and styles; update Product page layout

--- resources/js/Pages/Product.jsx
+++ resources/js/Pages/Product.jsx
@@ -1,6 +1,7 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import ProductDesc from "../components/Product/ProductDesc";
+import ProductDetails from "../components/Product/ProductDetails";
 import ProductSlide from "../components/Product/ProductSlide";
 
 export default function Product(){
@@ -9,6 +10,7 @@ export default function Product(){
             <Navbar/>
             <ProductSlide/>
             <ProductDesc/>
+            <ProductDetails/>
             <Footer/>
         </>
     )
