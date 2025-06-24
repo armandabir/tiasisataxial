81281800450c6151b9e4cde62cfe9f2fef607ba9
armandabir <81634618+armandabir@users.noreply.github.com>
2025-06-25 00:36:17 +0330

    Update product styles and layout; add checkmark image for product details

--- resources/js/components/Product/ProductDesc.jsx
+++ resources/js/components/Product/ProductDesc.jsx
@@ -4,14 +4,14 @@ export default function ProductDesc(){
     return (
         <section className={styles.productDesk} >
             <h1>نام مربوط بکیج</h1>
+            <h2 className={styles.title}>توضیحات کلی</h2>
             <div className={styles.general}>
-                <h2 className={styles.title}>توضیحات کلی</h2>
                 <p>
                     ورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد
                     ورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد
                 </p>
+                <BlueWhiteBg className="md:h-auto h-900 w-full absolute md:scale-x-110 md:top-0 -top-40 -scale-x-100 -rotate-4 -z-10" appimg/>
             </div>
-            <BlueWhiteBg className="h-4/5 absolute scale-x-110 -rotate-4 -z-10" appimg/>
         </section>
     )
 }
